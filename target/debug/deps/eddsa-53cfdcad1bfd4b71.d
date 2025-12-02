455c0f09213d47c1302a7177a2d29da683565178
mabingol <mabingol@gmail.com>
2025-12-02 09:38:53 +0000

    EdDsa jubjub, poseidon over BLS12381 scalar field

---
+++ target/debug/deps/eddsa-53cfdcad1bfd4b71.d
@@ -0,0 +1,5 @@
+/home/mabingol/project/EdDSA-Jubjub-mab/target/debug/deps/eddsa-53cfdcad1bfd4b71.d: packages/rust/eddsa/src/lib.rs
+
+/home/mabingol/project/EdDSA-Jubjub-mab/target/debug/deps/eddsa-53cfdcad1bfd4b71: packages/rust/eddsa/src/lib.rs
+
+packages/rust/eddsa/src/lib.rs:
